ce010b64dd52fef0b2234edf9c5889926b5859fd
vinayv_456 <vinayvallabineni456@gmail.com>
2021-04-02 11:34:00 +0530

    Add users endpoint for fetching all users

--- index.js
+++ index.js
@@ -6,18 +6,16 @@ const { pool } = require('./connection');
 
 
 
-app.get('/', async (req, res) => {
+app.get('/users', async (req, res) => {
+    let client = await pool.connect();
     try {
-        console.log(pool)
-        let client = await pool.connect();
-        let sql_query = `SELECT * FROM test`;
+        let sql_query = `SELECT * FROM users`;
         let result = await client.query(sql_query)
-        console.log(result.rows)
-        res.send(result.rows);
+        res.status(200).send(result.rows);
     } catch(e){
         throw("failed to fetch", e);
     }
-  })
+})
 
 app.listen(3000, ()=> {
     console.log('app is running on port 3000');
