73637035e13c26dbbd189da60e29d8e9b4f253d7
vinayv-456 <vinayvallabineni456@gmail.com>
2021-06-07 20:11:15 +0530

    db

--- connection.js
+++ connection.js
@@ -1,11 +1,15 @@
 const Pg = require('pg')
 
 const pool = new Pg.Pool({
-  user: 'postgres',
-  host: '127.0.0.1',
-  database: 'fr',
-  password: 'password',
-  port: 5432,
+  // user: 'postgres',
+  // host: '127.0.0.1',
+  // database: 'fr',
+  // password: 'password',
+  // port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 exports.pool = pool;
